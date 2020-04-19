60b5b612d32c055b0e30aa5f9a818cfe82c5e3ee
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-19 09:31:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end 
   
   post '/checkout' do 
-    session["item"] = params[:item_name]
+    session[:item] = params[:item_name]
     @session=session
     erb :show
   end 
